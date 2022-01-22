501163332840b638408d83b5e3873c054453a600
antonkulaga <antonkulaga@gmail.com>
2022-01-22 16:57:59 +0200

    first commit

---
+++ android/app/src/main/java/com/github/antonkulaga/MainActivity.java
@@ -0,0 +1,5 @@
+package com.github.antonkulaga;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.github.antonkulaga',
+  appName: 'codating',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
